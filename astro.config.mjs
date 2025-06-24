a531ee56801d6256bdb4f5c3422b8820a44c55ec
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2025-06-24 16:54:46 +0700

    Add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,28 +6,32 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'My Docs',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-          sidebar: [
-              {
-                  label: 'Python 🐍',
-                //   items: [
-                //       // Each item here is one entry in the navigation menu.
-                //       { label: 'Example Guide', slug: 'guides/example' },
-                //   ],
-                    autogenerate: { directory: 'Python' },
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
-          ],
-      }),
-	],
+    
+    base: '/void-codex/',
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    integrations: [
+        starlight({
+            title: 'My Docs',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+            sidebar: [
+                {
+                    label: 'Python 🐍',
+                    //   items: [
+                    //       // Each item here is one entry in the navigation menu.
+                    //       { label: 'Example Guide', slug: 'guides/example' },
+                    //   ],
+                        autogenerate: { directory: 'Python' },
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
+    ],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    
 });
